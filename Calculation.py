9f5b2ab2bd92de061d81208f6c4a31bf8a924a34
AlanJoji <92162970+AlanJoji@users.noreply.github.com>
2021-12-25 23:31:17 +0530

    Working on functions

--- Calculation.py
+++ Calculation.py
@@ -2,9 +2,61 @@
 
 
 #Modules 
-
+import string
 
 #Functions 
 
+def calculation (experssion) :
+
+    length = len(experssion)
+
+    for value in experssion : 
+
+        if (length <= 0) :
+            print("Error in input")
+            return(None)
+
+        elif (length == 1) :
+
+            if (type(check(value)) == int):
+                print("The number", value, "is returned")
+                return(value)
+
+            else : 
+                print("Error in input")
+                return(None)
+
+        elif (length == 2) :
+
+            
+
+        elif (length == 2) :
+
+
+def check (value) :
+
+    dictionary = { 0 : "0",
+    1 : "1",
+    2 : "2",
+    3 : "3",
+    4 : "4",
+    5 : "5",
+    6 : "6",
+    7 : "7",
+    8 : "8",
+    9 : "9",
+    "Addition" : "+",
+    "Subtraction" : "-",
+    "Multiplication" : "*",
+    "Division" : "/" }
+
+    if value in dictionary : 
+        return(dictionary.get(value))
+
+#def conversion (value) :
+
+
 
 #__main__
+
+experssion = input("Enter an expression : ")
